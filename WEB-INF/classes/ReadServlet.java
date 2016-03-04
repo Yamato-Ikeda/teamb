9d68c0f4117c43af4c61f49fcc946469fdcff06a
Yamato-Ikeda <dikteim@gmail.com>
2016-03-04 16:22:17 +0900

    掲示板READ　OK

--- WEB-INF/classes/ReadServlet.java
+++ WEB-INF/classes/ReadServlet.java
@@ -31,6 +31,9 @@ public class ReadServlet extends HttpServlet{
 		//CheckReplace cr = new CheckReplace();
 		//tb = cr.check(tb);
 		
+		ContentBean tester = (ContentBean)al.get(1);
+		System.out.println(tester.getUser_name());
+		
 		req.setAttribute("al",al);
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/practice.jsp");
 		dispatcher.forward(req,res);
