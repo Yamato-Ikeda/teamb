28e128212b8cbe65b6c971f0138cce6e95366728
Ryuji0516 <ro15304006@ga.tera-house.ac.jp>
2016-03-07 11:30:24 +0900

    dcd

---
+++ WEB-INF/classes/DeleteServlet.java
@@ -0,0 +1,44 @@
+import executer.*;
+
+import bean.*;
+
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DeleteServlet extends HttpServlet{
+	ReadExecuter RE=new ReadExecuter();
+	
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res)
+	throws IOException,ServletException{		
+		req.setCharacterEncoding("Windows-31J");
+		
+		
+		req.getParameter()
+
+		//int threadNo = Integer.parseInt( req.getParameter("thread") );
+		
+		//ContentsBean CB =new ContentsBean();
+		int postNo = -1;
+		ArrayList al = (ArrayList) RE.execute(postNo);
+		
+		//CheckReplace cr = new CheckReplace();
+		//tb = cr.check(tb);
+		
+		ContentBean tester = (ContentBean)al.get(1);
+		System.out.println(tester.getUser_name());
+		
+		req.setAttribute("al",al);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/Delete.jsp");
+		dispatcher.forward(req,res);
+		
+	}
+}
