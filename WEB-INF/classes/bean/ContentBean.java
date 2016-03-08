aaaa83e84ffbe17396bd4ed1c454dff995329849
Yamato-Ikeda <dikteim@gmail.com>
2016-03-08 10:34:08 +0900

    ちょっとコメント変更

--- WEB-INF/classes/bean/ContentBean.java
+++ WEB-INF/classes/bean/ContentBean.java
@@ -9,22 +9,8 @@ public class ContentBean{
 	private boolean delete_flag;
 	private String image;
 	private String date;
-<<<<<<< HEAD
-=======
-
 	
-	public void setPost_number(int post_number){
-		this.post_number = post_number;
-	}
-	public void setEmail_address(String email_address){
-		this.email_address = email_address;
-	}
-	//‚Â‚­‚è‚©‚¯
-
-		
->>>>>>> origin/master
-	
-	//‚¹‚Á‚½[
+	//‚¹‚Á‚½[ 
 	
 	public void setPost_number(int post_number){
 		this.post_number = post_number;
@@ -38,7 +24,6 @@ public class ContentBean{
 	public void setMessage(String message){
 		this.message = message;
 	}
-	
 	public void setDelete_key(String delete_key){
 		this.delete_key = delete_key;
 	}
@@ -63,7 +48,6 @@ public class ContentBean{
 	public String getEmail_address(){
 		return email_address;
 	}
-	
 	public String getMessage(){
 		return message;
 	}
@@ -80,5 +64,4 @@ public class ContentBean{
 		return date;
 	}
 	
-
 }
