c1fc18cf42b306741e975cbe44aa0682b2cd531c
Yamato-Ikeda <dikteim@gmail.com>
2016-03-08 11:06:43 +0900

    WriteServlet修正

--- WEB-INF/classes/WriteServlet.java
+++ WEB-INF/classes/WriteServlet.java
@@ -6,15 +6,12 @@ import java.util.ArrayList;
 
 import java.io.IOException;
 import java.io.File;
-
-import javax.servlet.http.Paet
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.Part;
 
 public class WriteServlet extends HttpServlet{
 	ReadExecuter RE=new ReadExecuter();
